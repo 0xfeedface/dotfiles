2a00f69f5446badd751f091a95b79cb1e74833ca
Norman Heino <norman.heino@gmail.com>
2023-05-24 08:40:46 +0200

    [emacs] add permutation theme
    A permutation of the default colors for light backgrounds.

--- dot_config/emacs/init.el
+++ dot_config/emacs/init.el
@@ -75,23 +75,7 @@
 (add-to-list 'major-mode-remap-alist '(c-mode . c-ts-mode))
 (add-to-list 'major-mode-remap-alist '(c++-mode . c++-ts-mode))
 
-;; Ad-hoc theme
-(custom-set-faces
- '(default ((t (:foreground "#111111" :background "#f8f8f8"))))
- '(font-lock-comment-face ((t (:foreground "#909090"))))
- '(font-lock-comment-delimiter-face ((t (:inherit 'font-lock-comment-face))))
- '(font-lock-string-face ((t (:foreground "ForestGreen")))) ;VioletRed4
- '(font-lock-builtin-face ((t (:foreground "sienna"))))
- '(font-lock-type-face ((t (:foreground "dark slate blue"))))
- '(font-lock-variable-name-face ((t (:inherit 'default))))
- '(font-lock-operator-face ((t (:inherit 'font-lock-keyword-face))))
- '(font-lock-negation-char-face ((t (:inherit 'font-lock-keyword-face))))
- '(font-lock-escape-face ((t (:inherit 'warning :bold nil))))
- '(font-lock-number-face ((t (:inherit 'font-lock-constant-face))))
- ;; '(font-lock--face ((t (:foreground ))))
- '(org-default ((t (:inherit 'default :font "Iosevka Custom Fixed Sans"))))
- '(org-block ((t (:font "Iosevka Custom Fixed Slab"))))
- )
+(load-theme 'default-dark t)
 
 (require 'package)
 (setq package-enable-at-startup nil)
---
+++ dot_config/emacs/themes/permutation-theme.el
@@ -0,0 +1,19 @@
+(deftheme permutation)
+(custom-theme-set-faces
+ 'permutation
+ '(default ((t (:foreground "#f8f8f8" :background "#111111"))))
+ '(font-lock-comment-face ((t (:foreground "#909090"))))
+ '(font-lock-comment-delimiter-face ((t (:inherit 'font-lock-comment-face))))
+ '(font-lock-string-face ((t (:foreground "ForestGreen")))) ;VioletRed4
+ '(font-lock-builtin-face ((t (:foreground "sienna"))))
+ '(font-lock-type-face ((t (:foreground "dark slate blue"))))
+ '(font-lock-variable-name-face ((t (:inherit 'default))))
+ '(font-lock-operator-face ((t (:inherit 'font-lock-keyword-face))))
+ '(font-lock-negation-char-face ((t (:inherit 'font-lock-keyword-face))))
+ '(font-lock-escape-face ((t (:inherit 'warning :bold nil))))
+ '(font-lock-number-face ((t (:inherit 'font-lock-constant-face))))
+ ;; '(font-lock--face ((t (:foreground ))))
+ '(org-default ((t (:inherit 'default :font "Iosevka Custom Fixed Sans"))))
+ '(org-block ((t (:font "Iosevka Custom Fixed Slab"))))
+ )
+(provide-theme 'permutation)
