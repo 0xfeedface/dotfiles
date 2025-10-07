93d9326adde615cbb88977b77ef5233fa9886bde
Norman Heino <norman.heino@deepl.com>
2025-10-07 12:16:10 +0200

    check for updates once a day

--- dot_config/nvim/lua/config/lazy.lua
+++ dot_config/nvim/lua/config/lazy.lua
@@ -31,7 +31,11 @@ require'lazy'.setup{
   -- colorscheme that will be used when installing plugins.
   -- install = { colorscheme = { 'habamax' } },
   -- automatically check for plugin updates
-  checker = { enabled = true },
+  checker = {
+    enabled = true,
+    notify = false,
+    frequency = 86400,
+  },
   ui = {
     icons = {
       cmd = '',
@@ -61,5 +65,5 @@ require'lazy'.setup{
   },
   change_detection = {
     notify = false
-  }
+  },
 }
